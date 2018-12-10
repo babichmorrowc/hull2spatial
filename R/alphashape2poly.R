b18d527394d8ae7c448c84f31b163245bc416c21
babichmorrowc <babichmorrowc@kenyon.edu>
2018-12-10 14:27:07 -0500

    starting to add roxygen comments

--- R/alphashape2poly.R
+++ R/alphashape2poly.R
@@ -1,26 +1,34 @@
+#' Convert an alpha shape object to a polygon.
+#'
+#' @param ashape An alpha shape object.
+#' @return The polygon matching the boundaries of the alpha shape.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+
 # Function to convert alpha shape to a polygon
 # Modified from https://rpubs.com/geospacedman/alphasimple
 
-ashape2poly <- function(hull){
+ashape2poly <- function(ashape){
   # Convert node numbers into characters
-  hull$ashape.obj$edges[,1] <- as.character(hull$ashape.obj$edges[,1])
-  hull_graph <- graph_from_edgelist(hull$ashape.obj$edges[,1:2], directed = FALSE)
-  if (!is.connected(hull_graph)) {
+  ashape$ashape.obj$edges[,1] <- as.character(ashape$ashape.obj$edges[,1])
+  ashape_graph <- graph_from_edgelist(ashape$ashape.obj$edges[,1:2], directed = FALSE)
+  if (!is.connected(ashape_graph)) {
     stop("Graph not connected")
   }
-  if (any(degree(hull_graph) != 2)) {
+  if (any(degree(ashape_graph) != 2)) {
     stop("Graph not circular")
   }
-  if (clusters(hull_graph)$no > 1) {
+  if (clusters(ashape_graph)$no > 1) {
     stop("Graph composed of more than one circle")
   }
   # Delete one edge to create a chain
-  cut_graph <- hull_graph - E(hull_graph)[1]
+  cut_graph <- ashape_graph - E(ashape_graph)[1]
   # Find chain end points
   ends = names(which(degree(cut_graph) == 1))
   path = get.shortest.paths(cut_graph, ends[1], ends[2])$vpath[[1]]
   # this is an index into the points
-  pathX = as.numeric(V(hull_graph)[path]$name)
+  pathX = as.numeric(V(ashape_graph)[path]$name)
   # join the ends
   pathX = c(pathX, pathX[1])
   return(pathX)
