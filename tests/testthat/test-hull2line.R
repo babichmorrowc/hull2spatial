e8e298928e17d342871d77399d78f58016b8b9ed
babichmorrowc <babichmorrowc@gmail.com>
2023-10-13 15:35:23 +0100

    Wrap up tests for all examples

--- tests/testthat/test-hull2line.R
+++ tests/testthat/test-hull2line.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("arc2line example return line", {
+  example_result <- arc2line(center = c(0.5, -0.1), r = 0.2, vector = c(0.1, 1), theta = 0.04)
+  expect_s4_class(example_result, "Line")
+})
+
+test_that("ahull2lines example return spatiallines", {
+  set.seed(123)
+  x <- matrix(runif(100), nc = 2)
+  ahull_02 <- alphahull::ahull(x, alpha = 0.2)
+  ahull_line_02 <- ahull2lines(ahull_02)
+  expect_s4_class(ahull_line_02, "SpatialLines")
 })
--- tests/testthat/test-hull2poly.R
+++ tests/testthat/test-hull2poly.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("ahull2poly example return spatialpolygon", {
+  iris_sepals <- iris[,1:2]
+  iris_sepals <- iris_sepals[!duplicated(paste(iris_sepals$Sepal.Length, iris_sepals$Sepal.Width)), ]
+  alphahull_1 <- alphahull::ahull(iris_sepals, alpha = 1)
+  hullpoly_1 <- ahull2poly(alphahull_1)
+  expect_s4_class(hullpoly_1, "SpatialPolygons")
 })
